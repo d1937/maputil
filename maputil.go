e8bac22fa27a549c5d8b8349bd340faf8a188eb3
samy <samy1090900@outlook.com>
2021-07-16 18:01:45 +0800

    first commit

---
+++ maputil.go
@@ -0,0 +1,10 @@
+package maputil
+
+func ArrayKeys(elements map[interface{}]interface{}) []interface{} {
+	i, keys := 0, make([]interface{}, len(elements))
+	for key, _ := range elements {
+		keys[i] = key
+		i++
+	}
+	return keys
+}
